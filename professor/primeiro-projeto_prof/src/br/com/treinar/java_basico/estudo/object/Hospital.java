8a1ca86899aff48ec7cf5d1f99d3ac4b91688d0f
Treinar <Treinar@SL02PROFESSOR>
2018-11-21 22:42:20 -0200

    construtor

--- professor/primeiro-projeto_prof/src/br/com/treinar/java_basico/estudo/object/Hospital.java
+++ professor/primeiro-projeto_prof/src/br/com/treinar/java_basico/estudo/object/Hospital.java
@@ -6,26 +6,36 @@ public class Hospital {
 		
 		Pessoa fulano = null;
 		
-		fulano = new Pessoa();
+		fulano = new Pessoa("Maria Sophia", 0.8F);
 		fulano.nome = "Maria Sophia";
-		fulano.altura = 0.8F;
-		System.out.println("Antes de crescer! " + fulano.altura);
+		//fulano.altura = 9.8F; atributo privado
+		fulano.crescer(0.8f);
+		fulano.peso = 30f;
+		System.out.println("Antes de crescer! " + fulano.recuperarAltura());
 		fulano.crescer();
-		System.out.println("Depois de crescer! " + fulano.altura);
+		System.out.println("Antes de crescer mais de um cm! " + fulano.recuperarAltura());
+		fulano.crescer(0.10f);
+		System.out.println("Depois de crescer! " + fulano.recuperarAltura());
+		
+		//recebendo retorno do metodo
+		float imcDeFulano = fulano.recuperarIMC();
+		System.out.println("IMC de Fulano: " + imcDeFulano);
 		
 		System.out.println(fulano.nome);
-		System.out.println(fulano.altura);
+		System.out.println(fulano.recuperarAltura());
 		
-		Pessoa tempo = new Pessoa();
+		Pessoa tempo = new Pessoa("Maria Sophia", 0.8F);
 		tempo.nome = "Emanuelle";
-		tempo.altura = 1.5F;
+//		tempo.altura = 1.5F; //atributo privado
+		tempo.crescer(1.5F);
 		
 		Pessoa xpto = fulano;
 		
 		System.out.println("Nome a partir da referencia xpto: " + xpto.nome);
-		xpto.altura = 0.81F;
+//		xpto.altura = 0.81F; //atributo privado
+		xpto.crescer(0.81F);
 		
-		System.out.println(fulano.altura);
+		System.out.println(fulano.recuperarAltura());
 		
 		fulano = null;
 		xpto = null;
--- professor/primeiro-projeto_prof/src/br/com/treinar/java_basico/estudo/object/Pessoa.java
+++ professor/primeiro-projeto_prof/src/br/com/treinar/java_basico/estudo/object/Pessoa.java
@@ -2,11 +2,62 @@ package br.com.treinar.java_basico.estudo.object;
 
 public class Pessoa {
 
-	float altura;
-	String nome;
+	private String dataNascimento;
+	private float altura;//modificador privado so pode ser acessado na propria classe
+	float peso;//modificador default pode ser acessado na classe e nas classes do mesmo pacote
+	public String nome;//pode ser acessado em qualquer ponto do projeto.
 	
-	void crescer() {
+	public Pessoa(String dataNascimento, float tamanho) {
+		this.dataNascimento = dataNascimento;
+		this.crescer(tamanho);
+	}
+	
+	public Pessoa(String dataNascimento, float tamanho, String nome) {
+		this(dataNascimento, tamanho);
+		this.nome = nome;
+	}
+
+	/**
+	 * 
+	 */
+	public void crescer() {
 		altura += 0.01F;
 	}
+	
+	/**
+	 * Aumenta o tamanho da pessoa blah blah blah
+	 * @param tamanhoEmCentimetros tamanho que a pessoa cresceu
+	 */
+	public void crescer(float tamanhoEmCentimetros) {
+		altura += tamanhoEmCentimetros;
+	}
+	
+	/*
+	 * eu programador quero escrever um texto grande
+	 * para documentar meu codigo
+	 * */
+	/**
+	 * 
+	 * @return retorna o indice de massa corporal baseado na formula
+	 * peso x altura ao quadrado dividido por dois
+	 */
+	public float recuperarIMC() {//eu programador quero entender esta linha
+		float imc = peso * (altura * altura) / 2;
+		return imc;
+	}
+
+	public float recuperarAltura() {
+		return altura;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
 	
 }
