b520d0895133ee2a38db4e7e89cc0c1a725395a2
Treinar <Treinar@SL02PROFESSOR>
2018-11-23 22:16:36 -0200

    static

--- itau/src/br/com/itau/modelo/ContaCorrente.java
+++ itau/src/br/com/itau/modelo/ContaCorrente.java
@@ -2,6 +2,22 @@ package br.com.itau.modelo;
 
 public class ContaCorrente extends Conta {
 
+	private static final double VALOR_SAQUE;
+	
+	/**
+	 * bloco de inicializacao de atributos static, executado apenas 
+	 * uma vez, quando a JVM conhece a classe
+	 */
+	static {
+		VALOR_SAQUE = 1;
+	}
+	
+	static {
+		//posso ter varios blocos de inicializacao de atributos static
+		//estes serao executados respeitando a regra a cima, na ordem
+		//em que foram declarados.
+	}
+	
 	private double taxaManutencao;
 
 	public double getTaxaManutencao() {
@@ -17,8 +33,8 @@ public class ContaCorrente extends Conta {
 		 * regra de negocio cobra-se um real por saque
 		 * para sacar um valor n o saldo devera ser n + 1
 		*/
-		if (valor > 0 && saldo >= (valor + 1)) {
-			saldo -= valor + 1;
+		if (valor > 0 && saldo >= (valor + VALOR_SAQUE)) {
+			saldo -= valor + VALOR_SAQUE;
 		}
 	}
 
